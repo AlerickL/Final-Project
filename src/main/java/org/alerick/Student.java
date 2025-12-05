157b922aeb281bdd761b51397a66e00c0c434ff8
AlerickL <alericklanglois@gmail.com>
2025-12-05 17:04:40 -0500

    Add registerCourse method

--- src/main/java/org/alerick/Student.java
+++ src/main/java/org/alerick/Student.java
@@ -1,5 +1,6 @@
 package org.alerick;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
@@ -8,8 +9,30 @@ public class Student {
     private Gender gender;
     private Address address;
     private Department department;
-    private List<Course> registeredCourse;
-    private static int nextId;
+    private List<Course> registeredCourses;
+    private static int nextId = 1;
+
+    /**
+     * Registers a new course for a student:
+     * i. adds the course to the student's registered courses.
+     * ii. adds the student to the course's registered students.
+     * iii. adds a null to each assignment in the course.
+     * @param course the course.
+     * @return if the student was registered in the course
+     */
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        } else {
+            registeredCourses.add(course);
+            course.registerStudent(this);
+            List<Assignment> assignments = course.getAssignments();
+            for (int i = 0; i < assignments.size(); i++) {
+                assignments.get(i).getScores().add(null);
+            }
+            return true;
+        }
+    }
 
 
 
