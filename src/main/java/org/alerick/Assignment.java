cbc909d1c29f6e7d7edce9f3913f8611028c4db7
AlerickL <alericklanglois@gmail.com>
2025-12-05 16:39:13 -0500

    Add fields

--- src/main/java/org/alerick/Assignment.java
+++ src/main/java/org/alerick/Assignment.java
@@ -1,4 +1,27 @@
 package org.alerick;
 
+import java.util.List;
+import java.util.Random;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private List<Integer> scores;
+    private static int nextId = 1;
+
+    private void generateRandomScores() {
+        Random random = new Random();
+        int idx = random.nextInt(0, 11);
+
+        Integer score = 0;
+        score = switch (idx) {
+            case 1, 2 -> random.nextInt(60, 70);
+            case 3, 4 -> random.nextInt(70, 80);
+            case 5, 6, 7, 8 -> random.nextInt(80, 90);
+            case 9, 10 -> random.nextInt(90, 101);
+            default -> random.nextInt(0, 60);
+        };
+
+    }
 }
--- src/main/java/org/alerick/Student.java
+++ src/main/java/org/alerick/Student.java
@@ -1,4 +1,20 @@
 package org.alerick;
 
+import java.util.List;
+
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registeredCourse;
+    private static int nextId;
+
+
+
+
+    public enum Gender {
+        MALE, FEMALE
+    }
 }
