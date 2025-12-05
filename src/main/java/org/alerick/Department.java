e02f0a642ccbfb92d63b62cdc4d2f80a7de82f50
AlerickL <alericklanglois@gmail.com>
2025-12-05 16:03:38 -0500

    Add Department class

---
+++ src/main/java/org/alerick/Department.java
@@ -0,0 +1,37 @@
+package org.alerick;
+
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextid = 1;
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)){
+            this.departmentName = departmentName;
+            this.departmentId = String.format("D%02d", nextid++);
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
+
+    /**
+     * checks if a department name is valid (contains only letters and spaces)
+     * @param departmentName the department name
+     * @return if the department name is valid
+     */
+    private static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isBlank()) {
+            return false;
+        }
+        char[] chars = departmentName.toCharArray();
+
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
